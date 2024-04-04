5941ef9a68db987a4003c05ce4586fcc654d39a8
Jagger Gourley <jaggergourley@gmail.com>
2024-04-04 11:20:09 -0500

    Working translation both ways, adding more symbols

--- morse.c
+++ morse.c
@@ -27,16 +27,20 @@ struct morse_entry {
 
 // array of structs to store char-morse mappings
 struct morse_entry morse_table[] = {
-    {'A', ".-"},    {'B', "-..."},  {'C', "-.-."},  {'D', "-.."},
-    {'E', "."},     {'F', "..-."},  {'G', "--."},   {'H', "...."},
-    {'I', ".."},    {'J', ".---"},  {'K', "-.-."},  {'L', ".-.."},
-    {'M', "--"},    {'N', "-."},    {'O', "---"},   {'P', ".--."},
-    {'Q', "--.-"},  {'R', ".-."},   {'S', "..."},   {'T', "-"},
-    {'U', "..-"},   {'V', "...-"},  {'W', ".--"},   {'X', "-..-"},
-    {'Y', "-.--"},  {'Z', "--.."},  {'1', ".----"}, {'2', "..---"},
-    {'3', "...--"}, {'4', "....-"}, {'5', "....."}, {'6', "-...."},
-    {'7', "--..."}, {'8', "---.."}, {'9', "----."}, {'0', "-----"},
-};
+    {'A', ".-"},      {'B', "-..."},   {'C', "-.-."},   {'D', "-.."},
+    {'E', "."},       {'F', "..-."},   {'G', "--."},    {'H', "...."},
+    {'I', ".."},      {'J', ".---"},   {'K', "-.-"},    {'L', ".-.."},
+    {'M', "--"},      {'N', "-."},     {'O', "---"},    {'P', ".--."},
+    {'Q', "--.-"},    {'R', ".-."},    {'S', "..."},    {'T', "-"},
+    {'U', "..-"},     {'V', "...-"},   {'W', ".--"},    {'X', "-..-"},
+    {'Y', "-.--"},    {'Z', "--.."},   {'1', ".----"},  {'2', "..---"},
+    {'3', "...--"},   {'4', "....-"},  {'5', "....."},  {'6', "-...."},
+    {'7', "--..."},   {'8', "---.."},  {'9', "----."},  {'0', "-----"},
+    {'.', ".-.-.-"},  {',', "--..--"}, {'?', "..--.."}, {'\'', ".----."},
+    {'!', "-.-.--"},  {'/', "-..-."},  {'(', "-.--."},  {')', "-.--.-"},
+    {'&', ".-..."},   {':', "---..."}, {';', "-.-.-."}, {'=', "-...-"},
+    {'+', ".-.-."},   {'-', "-....-"}, {'_', "..-.-"},  {'"', ".-..-."},
+    {'$', "...-..-"}, {'@', ".--.-."}, {' ', "/"}};
 
 int main() {
   char selection;
@@ -63,20 +67,21 @@ int main() {
 }
 
 char *morse_input() {
-  char *input = malloc(100 * sizeof(char));
+  char *input = malloc(256 * sizeof(char));
   if (input == NULL) {
     fprintf(stderr, "Memory allocation failed\n");
     exit(1);
   }
 
-  printf("Enter Morse code ('.' for dot, '-' for dash,\nusing spaces between "
-         "letters and '/' between words): ");
+  printf("Enter Morse code ('.' for dot, '-' for dash, using spaces between\n"
+         "letters and '/' between words, be sure to put spaces around '/' as\n"
+         "well): ");
 
   // Consume char in input buffer
   while (getchar() != '\n')
     ;
 
-  fgets(input, 100, stdin);
+  fgets(input, 256, stdin);
   printf("Morse input before newline remove: %s\n", input);
 
   // Remove newline character from input
@@ -95,14 +100,14 @@ char *morse_input() {
 }
 
 char *text_input() {
-  char *input = malloc(100 * sizeof(char));
+  char *input = malloc(256 * sizeof(char));
 
   printf("Enter text (alphanumeric): ");
 
   // Consume char in input buffer
   while (getchar() != '\n')
     ;
-  fgets(input, 100, stdin);
+  fgets(input, 256, stdin);
 
   // Remove newline character from input
   input[strcspn(input, "\n")] = '\0';
@@ -143,7 +148,7 @@ char decode_morse_token(const char *token) {
 
 // should be return a pointer to char (string)
 char *morse_to_text(char *morse) {
-  char *decoded_message = malloc(100 * sizeof(char));
+  char *decoded_message = malloc(256 * sizeof(char));
   char *token = strtok(morse, " ");
 
   while (token != NULL) {
@@ -169,11 +174,31 @@ char *morse_to_text(char *morse) {
   return decoded_message;
 }
 
+const char *decode_text_char(const char character) {
+  for (size_t i = 0; i < sizeof(morse_table) / sizeof(morse_table[0]); i++) {
+    // printf("in the loop looking for char match: %zu\n", i);
+    if (toupper(character) == morse_table[i].character) {
+      printf("Decoded morse: %s\n", morse_table[i].morse_code);
+      // Return corresponding morse
+      return morse_table[i].morse_code;
+    }
+  }
+  // Unmatched character
+  return "?";
+}
 char *text_to_morse(char *text) {
-  char *decoded_message = malloc(100 * sizeof(char));
+  char *decoded_message = malloc(256 * sizeof(char));
 
-  // will be reading input string one char at a time
-  // find equivalent morse code for char and append to decoded string
+  // Longest we will consider is 6 chars
+  char *decoded_char = malloc(6 * sizeof(char));
+
+  for (int i = 0; text[i]; i++) {
+    decoded_char = (char *)decode_text_char(text[i]);
+    printf("Decoded char: %s\n", decoded_char);
+    strcat(decoded_message, decoded_char);
+    strcat(decoded_message, " "); // Append space between characters
+    printf("Current decoded message from text_to_morse: %s\n", decoded_message);
+  }
 
   return decoded_message;
 }
